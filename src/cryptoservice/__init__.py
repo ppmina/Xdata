3b8ac991cd95904e8ffe9b5a8f6e734c5a6dae25
min <minzzzai.s@gmail.com>
2024-12-29 16:21:25 +0800

    feat: add demo func

--- src/cryptoservice/__init__.py
+++ src/cryptoservice/__init__.py
@@ -1,7 +1,7 @@
 """Cryptocurrency trading bot package."""
 
-__version__ = "0.1.0"
-__author__ = "Your Name"
+__version__ = "0.1.1"
+__author__ = "Minnn"
 
 # 可以在这里导出常用的模块，使得用户可以直接从包根导入
 from .client import BinanceClientFactory
